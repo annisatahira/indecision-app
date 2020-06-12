261c79ccae439d9d6bdc79af3a3b708fd56123a3
annisatahira <annisatahira999@gmail.com>
2020-06-12 12:33:00 +0700

    exploring jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,9 @@
 
 console.log("hi"); // JavaScript XML
 
-var template = /*#__PURE__*/React.createElement("p", null, "Cool!");
+var template = /*#__PURE__*/React.createElement("p", null, "Cool!"); //challenge
+
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Annisa Tahira"), /*#__PURE__*/React.createElement("p", null, "Nickname : Anta"), /*#__PURE__*/React.createElement("p", null, "Location : Indonesia"));
 var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot); // use to preset
+ReactDOM.render(templateTwo, appRoot); // use to preset
 // babel src/app.js --out-file=public/scripts/app.js --presets=env, react
--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@ console.log("hi");
 
 // JavaScript XML
 var template = <p>Cool!</p>;
+
+//challenge
+var templateTwo = (
+  <div>
+    <h1>Annisa Tahira</h1>
+    <p>Nickname : Anta</p>
+    <p>Location : Indonesia</p>
+  </div>
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
 
 // use to preset
 // babel src/app.js --out-file=public/scripts/app.js --presets=env, react
