ab92b9cb6ab21606cbe7142f0d1d6fa11f7132ee
annisatahira <annisatahira999@gmail.com>
2020-06-21 12:38:31 +0700

    styling widget header

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -3,13 +3,16 @@ import Option from "./Option.jsx";
 
 const Options = props => (
   <div>
-    <button
-      className="button button--link"
-      onClick={props.handleRemoveAll}
-      disabled={!props.hasOptions}
-    >
-      Remove All Option
-    </button>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Here is your option</h3>
+      <button
+        className="button button--link"
+        onClick={props.handleRemoveAll}
+        disabled={!props.hasOptions}
+      >
+        Remove All Option
+      </button>
+    </div>
     {props.options.length === 0 && <p>Please Add some option</p>}
     {props.options.map(option => (
       <Option
